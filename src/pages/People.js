8880aee47f1d231fd59822975ce70b8f31efab74
delmarNK <delmarjinku3@gmail.com>
2024-03-29 12:53:50 +0800

    made the fetch on people.js and updated app.css

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,9 +1,42 @@
-
+import { useState,useEffect } from "react"
 
 function People() {
-  return (
-    <div>People</div>
-  )
-}
+    const [people,setPeople] = useState([])
+    const URL = process.env.REACT_APP_API_URL + '/people'
+    function getPeople(){
+        try{
+            fetch(URL)
+            .then((res)=>res.json())
+            .then((data)=>setPeople(data))
+        } catch(error){
+            console.log(error)
+        }
+    }
+    useEffect(()=>{
+        getPeople()
+    },[])
+    function loading(){
+        return <section className="peopleList"><h1>Hold on a sec...
+            <span>
+                <img className="spinner" src="https://freesvg.org/img/1544764567.png" />
+            </span>
+         </h1>
+        </section>
+    }
+    function loaded(){
+        return (
+            <section className="peopleList">
+                {people.map((person)=>(
+                    <div>
+                        <h1>{person.name}</h1>
+                        <img src={person.image}/>
+                        <p>{person.title}</p>
+                    </div>
+                ))}
+            </section>
+          )
+    }
+    return  people ? loaded() : loading()
+}  
 
 export default People
